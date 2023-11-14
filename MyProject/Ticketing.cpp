a6c053e768755bb730fd88dfd2e60925f7a89a47
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2023-11-14 16:08:23 +0200

    add unique id

--- MyProject/Ticketing.cpp
+++ MyProject/Ticketing.cpp
@@ -14,7 +14,12 @@ private:
     string eventName;
     string eventDate;
     string eventTime;
+
+    static int uniqueIdCounter; // Static member to maintain a unique ID counter
+    int ticketId; // Member variable for the ticket ID
 };
+
+int TicketManager::uniqueIdCounter = 1; // Initialize static member variable
 
 int main() {
 	cout << endl << "Hello World, updated .cpp file!";
