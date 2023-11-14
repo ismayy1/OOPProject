16a299c3a2e27d4e4fb6fd90b2394ee34191969b
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2023-11-14 16:02:47 +0200

    creating Class Variables

--- MyProject/Ticketing.cpp
+++ MyProject/Ticketing.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+
+class TicketManager {
+private:
+    int maxSeats;
+    int numRows;
+    int numZones;
+    int seatsPerRow;
+
+    string eventName;
+    string eventDate;
+    string eventTime;
+};
 
 int main() {
 	cout << endl << "Hello World, updated .cpp file!";
