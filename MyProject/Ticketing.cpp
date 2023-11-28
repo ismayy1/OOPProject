f3bfbcb20d3056abc0e0cd51dc5f0456842091fb
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2023-11-28 12:05:23 +0200

    error corrected, (+,=,!,>,>>,<<) operators are declared

--- MyProject/Ticketing.cpp
+++ MyProject/Ticketing.cpp
@@ -89,17 +89,47 @@ public:
         this->setMovieNames(ticket.movieNames, ticket.noMovies);
     }
     //overloaded operators
-    Ticket operator=(const Ticket& ticket) {
+    void operator=(const Ticket& ticket) {
         if (&ticket == this) {
             return;
         }
         delete[] this->movieNames;
         this->setMovieNames(ticket.movieNames, ticket.noMovies);
-        return ticket;
     }
     //destructor
     ~Ticket() {
 
+    }
+
+    //input stream operator
+
+    friend ostream& operator<<(ostream& console, Ticket& t);
+
+    void operator>>(istream& console) {
+        cout << endl << "Room number is: ";
+        console >> this->roomNum;
+        cout << endl << "Row number: ";
+        console >> this->rowNum;
+        cout << endl << "Seat number: ";
+        console >> this->seatNum;
+    }
+
+    //math operator
+    Ticket operator+(int value) {
+        Ticket t = *this;
+        t.roomNum += value;
+        return t;
+    }
+    bool operator!() {
+        return this->noMovies > 0;
+    }
+    bool operator>(int value) {
+        if (this->roomNum > value) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 };
 
@@ -227,7 +257,7 @@ public:
     }
     void setMovieName(const char* newMovieName) {
         delete[] this->movieName;
-        this->movieName = new char[strlen(this->movieName) + 1];
+        this->movieName = new char[strlen(newMovieName) + 1];
         strcpy(this->movieName, newMovieName);
     }
     void setTime(const string& newTime) {
@@ -264,64 +294,6 @@ public:
     }
 };
 
-
-
-
-
-
-class TicketManager {
-private:
-    int maxSeats = 0;
-    int numRows = 0;
-    int numZones = 0;
-    int seatsPerRow = 0;
-
-    char* eventName = nullptr;
-    string eventDate = "";
-    string eventTime = "";
-
-    static int uniqueIdCounter; // Static member to maintain a unique ID counter
-    const int ticketId; // Member variable for the ticket ID
-
-public:
-    TicketManager() :ticketId(0) {}
-
-    TicketManager(int ticketId, int maxSeats, int numRows, int numZones, int seatsPerRow, char* eventName, string eventDate, string eventTime)
-        :ticketId(ticketId) {
-
-    }
-
-    void setMaxSeats(int newMaxSeats) {
-        this->maxSeats = newMaxSeats;
-    }
-    void setNumRows(int newNumRows) {
-        this->numRows = newNumRows;
-    }
-    void setNumZones(int newNumZones) {
-        this->numZones = newNumZones;
-    }
-    void setSeatsPerRow(int newSeatsPerRow) {
-        this->seatsPerRow = newSeatsPerRow;
-    }
-    void setEventName(const char* newEventName) {
-        delete[] this->eventName; //prevent memory leak
-        this->eventName = new char[strlen(newEventName) + 1];
-        strcpy(this->eventName, newEventName);
-    }
-    void setEventDate(const string& newEventDate) {
-        this->eventDate = newEventDate;
-    }
-    void setEventTime(const string& newEventTime) {
-        this->eventTime = newEventTime;
-    }
-
-    ~TicketManager() {
-
-    }
-
-};
-
-int TicketManager::uniqueIdCounter = 1; // Initialize static member variable
 
 int main() {
 	cout << endl << "Hello World, updated .cpp file!";
